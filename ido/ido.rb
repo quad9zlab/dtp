8f8bf23436f1a38a258c60958f8ad3c04b8f2a83
Nobuyuki Takahiro <studio.quad9@gmail.com>
2017-07-10 21:28:05 +0900

    ido add comment

--- ido/ido.rb
+++ ido/ido.rb
@@ -6,37 +6,43 @@ require '../lib/iString'
 require '../lib/iArray'
 require '../lib/iHash'
 
+# ディレクトリ内の該当テキストファイルを収集する。
 files = Dir.glob("*.txt")
+# ido.rb用に調整したYAMLに変換する。
 files.txt2yml
+# ディレクトリ内の該当YAMLファイルを収集する。
 yamls = Dir.glob("*.yml")
+# 変数の初期化。形成したデータを配列に格納する。
 form = []
+# YAMLをIndesignデータ結合用のCSVに変換する。
 yamls.each do |y|
   orgfn = File.basename(y, ".yml")
-  member_info = YAML.load_file(y)
-  # name_ruby = member_info.rubyfuri
-  p member_info.photonawa
-  # member_info.each do |k, v|
-  #   form << [name_ruby.shift,
-  #            k,
-  #            v["date"],
-  #            v["office"],
-  #            v["pcode"],
-  #            v["zip1"],
-  #            v["zip2"],
-  #            v["tel"],
-  #            v["fax"],
-  #           ]  # photo_name.shift
-  # end
-  # # CSV用にヘッダーを入れた配列を下準備する。
-  # my_headers = %w[name_ruby id date office pcode zip1 zip2 tel fax @psd]
-  # # CSV用にインスタンスを作成。
-  # contents = CSV.generate("", :headers => my_headers, :write_headers => true) do |csv|
-  #   form.each { |i| csv << i }
-  # end
-  # # CSVファイルとして書き込む。
-  # File.open("#{ orgfn }.csv", 'w+:UTF-16:UTF-8') do |f|
-  #   f.puts contents
-  # end
+  member = YAML.load_file(y)
+  # iHash#rubyfuriメソッド
+  name_ruby = member.rubyfuri
+  # iHash#rubyfuriメソッド
+  photo_name = member.photonawa
+  member.each do |k, v|
+    form << [name_ruby.shift,
+             k,
+             v["date"],
+             v["office"],
+             v["pcode"],
+             v["zip1"],
+             v["zip2"],
+             v["tel"],
+             v["fax"],
+             photo_name.shift
+            ]
+  end
+  # CSV用にヘッダーを入れた配列を下準備する。
+  my_headers = %w[name_ruby id date office pcode zip1 zip2 tel fax @photo_name]
+  # CSV用にインスタンスを作成。
+  contents = CSV.generate("", :headers => my_headers, :write_headers => true) do |csv|
+    form.each { |i| csv << i }
+  end
+  # CSVファイルとして書き込む。
+  File.open("#{ orgfn }.csv", "w+:UTF-16:UTF-8") do |f|
+    f.puts contents
+  end
 end
-
-exit
