7401aeda2e3102d3d315d8247c8f20693636e3b5
Nobuyuki Takahiro <studio.quad9@gmail.com>
2017-07-12 18:21:00 +0900

    test complete

--- test/txto.rb
+++ test/txto.rb
@@ -1,7 +1,20 @@
 # coding: utf-8
 require "nkf"
 
+module AddMethod
+  def empty_line!
+
+    # ############### 問題発生 ###############
+    # 下記の検索置換は効くが空行削除が作用しないのは何故か？
+    # 答えは、先頭位置でなく改行を入れていないから。
+    # self.gsub!(/^\s*/,'')
+
+  end
+end
+
+
 class String
+  include AddMethod
 
   def clean_char!
     #=> 全角の「！」から「｝」までの連続した文字のうち
@@ -17,13 +30,11 @@ class String
     self.gsub!(/(?<=\d)，(?=\d{3})/,'\1,\2')
     # 数字の桁区切りを変換してから、読点、句点を変換する。
     self.tr!("，", "、")
-    self.tr!("．", "。") unless self =~ /(?<=\w)．/
+    self.tr!("．", "。")
     # 時間表示。時間と分を区切る記号を全角に変換する。
     self.gsub!(/(?<!\d|\.)(\d{1,2})(?!\d|,|\.):(?<!\d|\.)(\d{2})(?!\d|,|\.)/, '\1：\2')
     #### nkfで文字列の変換
     ## オプション
-    ## 『-w』
-    # UTF-8で出力するオプション
     ## 『-m0』
     # MIME decode/encodeのためのオプション。
     # nkf はデフォルトでMIME encoded-wordのデコードを試みるため、
@@ -33,28 +44,4 @@ class String
     # この動作を望まない場合は-xを指定する。
     NKF.nkf("-wXm0", self)
   end
-
-  def empty_line!
-    # ############### 問題発生 ###############
-    # 下記の検索置換は効くが空行削除が作用しないのは何故か？
-    # 答えは、先頭位置でなく改行を入れていないから。
-    self.gsub!(/^\n/, "")
-  end
-
-  # 氏名を5文字で整理する。
-  def namaezoroe!
-    sei_mei = self.split("　")
-    sei = sei_mei[0]
-    mei = sei_mei[1]
-    sei_s = sei_mei[0].size
-    mei_s = sei_mei[1].size
-    if sei_s >= 3 && mei_s >= 2 || sei_s >= 2 && mei_s >= 3
-      "#{ sei }#{ mei }"
-    elsif sei_s == 1 && mei_s == 2 || sei_s == 2 && mei_s == 1
-      "#{ sei }　　#{ mei }"
-    else
-      "#{ sei }　#{ mei }"
-    end
-  end
-
 end
